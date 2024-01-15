651cefac7aeb27ed3e8c95d1b46b755f6a1179a9
daniel <d.danielprince2003@gmail.com>
2024-01-15 19:33:43 +0530

    feat:added java file

---
+++ javaS.js
@@ -0,0 +1,75 @@
+let a = {
+    name : "daniel" ,
+    age : "20" ,
+    greet : function()
+    {
+        console.log("Hi My Name is " + this.name + " ,", this.age,"yrs old");
+    } 
+}
+a.greet()
+
+let create = {
+    create(n="Name",a="Age") 
+    {
+        this.name = n;
+        this.age = a;
+    },
+    getPerson: function()
+    {
+        console.log(`Hi Iam ${this.name} My age is ${this.age}`);
+    } 
+}
+
+function Employee(n,a,s)
+{
+    this.name = n
+    this.age = a
+    this.sal = s
+    this.getDetail = function () 
+    {
+        console.log("\n ",this.name,"\t",this.age,"\t",this.sal);
+    }
+}
+
+let daniel = new Employee("daniel",20,20000)
+daniel.getDetail()
+daniel.name = "doflamingo"
+daniel.getDetail()
+
+let obj = new String("daniel")
+console.log(obj);
+
+const person = 
+{
+    name : "daniel",age : 30, sal : 90000
+}
+
+for(keys of Object.entries(person))
+{
+    console.log(keys);
+
+}
+
+duplicate = Object.assign(0,person)
+duplicate.name = "David"
+for(i of Object.entries(duplicate))
+{
+    console.log(i);
+}
+
+console.log(Math.random()*10);
+
+num = [1,2,3,4,5]
+console.log(...num);
+
+console.log(Math.max(...num));
+
+// String
+
+fname = "daniel"
+console.log(fname.concat(" ","prince"));
+
+console.log(fname.split(''));
+
+console.log(eval("1+4"));
+console.log("1"+1);
