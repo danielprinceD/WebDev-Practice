56f590c88ff66a1c0dc5a6f32bae522a3bfcd150
danielprinceD <d.danielprince2003@gmail.com>
2024-03-02 09:24:39 +0530

    feat:Company website Updated

--- Company/src/header.tsx
+++ Company/src/header.tsx
@@ -3,8 +3,15 @@ import Image from './assets/comp.jpg'
 import { useState } from 'react';
 
 
+
 function Header() {
+  const [state , makeState] = useState(true)
+  const [state1 , makeState1] = useState(false)
+  const [state2 , makeState2] = useState(false)
+  const [state3 , makeState3] = useState(false)
+  let theme1 = state ? " marked" : " "
   
+  let themeing = ()=> state ?  makeState(false) : makeState(true)
   return (
     <>
       <header className="container">
@@ -13,8 +20,9 @@ function Header() {
         </div>
         <div className="list">
           <ul>
-            <li className='marked listing'>Home</li>
-            <li className='listing'> Contact</li>
+            
+            <li className={'listing'+ theme1}onClick={themeing}>Home</li>
+            <li className={'listing' } onClick={themeing} > Contact</li>
             <li className='listing'>About</li>
             <li className='listing'>Services</li>
           </ul>
